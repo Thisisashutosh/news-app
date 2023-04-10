a42d3fa4abb0b11c80e68a09eb05cc9b77915c64
Ashutosh <thisisashutoshdubey@gmail.com>
2023-04-10 20:22:42 +0530

    final commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,105 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export class Navbar extends Component {
-
-
-    render() {
-        return (
-            <div>
-                <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-                    <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">NewsMonkey</Link>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/business">Business</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/health">Health</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/science">Science</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/sports">Sports</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/technology">Technology</Link>
-                                </li>
-                                <li class="nav-item dropdown mx-30">
-                                    <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        Choose country
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a class="dropdown-item" href="/">Australia</a></li>
-                                        <li><a class="dropdown-item" href="/">USA</a></li>
-                                        <li><hr class="dropdown-divider"/></li>
-                                        <li><a class="dropdown-item" href="/">India</a></li>
-                                    </ul>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
+  render() {
+    return (
+      <div>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/">
+              NewsMonkey
+            </Link>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/business">
+                    Business
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/entertainment">
+                    Entertainment
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health">
+                    Health
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/science">
+                    Science
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/sports">
+                    Sports
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/technology">
+                    Technology
+                  </Link>
+                </li>
+                <li class="nav-item dropdown mx-30">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="/"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Choose country
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        Australia
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        USA
+                      </a>
+                    </li>
+                    <li>
+                      <hr class="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="/">
+                        India
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
             </div>
-        )
-    }
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
